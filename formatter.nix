3ebe3dae7edd170847565b120d4de83c800d3fb2
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2025-04-19 18:56:09 +0000

    verion inspired by https://github.com/zimbatm/home/blob/main/packages/formatter/default.nix

--- formatter.nix
+++ formatter.nix
@@ -5,15 +5,41 @@
   ...
 }:
 let
-  treefmtEval = inputs.treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
-  formatter = treefmtEval.config.build.wrapper;
-  foo = 1;
+  formatter = inputs.treefmt-nix.lib.mkWrapper pkgs {
+    _file = __curPos.file;
+    imports = [ ./treefmt.nix ];
+  };
+
+  check =
+    pkgs.runCommand "format-check"
+      {
+        nativeBuildInputs = [
+          formatter
+          pkgs.git
+        ];
+      }
+      ''
+        export HOME=$NIX_BUILD_TOP/home
+
+        # keep timestamps so that treefmt is able to detect mtime changes
+        cp --no-preserve=mode --preserve=timestamps -r ${flake} source
+        cd source
+        git init --quiet
+        git add .
+        treefmt --no-cache
+        if ! git diff --exit-code; then
+          echo "-------------------------------"
+          echo "aborting due to above changes ^"
+          exit 1
+        fi
+        touch $out
+      '';
 in
 formatter
 // {
   meta = formatter.meta // {
     tests = {
-      check = treefmtEval.config.build.check flake; # Check the whole repo
+      check = check;
     };
   };
 }
